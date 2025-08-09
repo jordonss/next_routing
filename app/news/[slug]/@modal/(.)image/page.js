a03cebe48690bec765c1872e6fee5a5a0dfd6a02
Artem <nemezg@gmail.com>
2025-08-09 20:49:10 +0300

    interception_modal

---
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -0,0 +1,23 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
+
+export default function InterceptedImagePage({ params }) {
+  const newsItemSlug = params.slug;
+
+  const newsItem = DUMMY_NEWS.find((item) => item.slug === newsItemSlug);
+
+  if (!newsItem) {
+    notFound(); // This will trigger the not-found.js page
+  }
+
+  return (
+    <>
+      <div className="modal-backdrop" />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
+    </>
+  );
+}
