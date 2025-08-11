2eedd6ba90dd23a5ce4bfa9bbd59309df5ba2fb2
Artem <nemezg@gmail.com>
2025-08-11 21:36:33 +0300

    Refactor NewsPage component to use async/await for data fetching and remove unnecessary state management

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,50 +1,18 @@
-"use client";
-
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    async function fetchData() {
-      setLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-			// Check if the response is ok (status in the range 200-299)
-      if (!response.ok) {
-        setError("Failed to fetch news");
-        setLoading(false);
-        return; // Stop further processing
-      }
-
-      const data = await response.json();
-      setLoading(false);
-			setNews(data);
-      
-    }
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
   }
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
-
-  let newContent;
-
-  if (news) {
-    newContent = <NewsList news={news} />;
-  }
+  const news = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {newContent}
+      <NewsList news={news} />
     </>
   );
 }
